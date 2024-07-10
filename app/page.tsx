ee6af76aaffcbd774fb89b52f317af043d41042e
김동우 <t6u80o@naver.com>
2024-07-10 21:33:48 +0900

    f: promise.all 을 통한 병렬적인 프로미스 처리 및 db 데이터 검증을 우선적으로 처리함으로 openAI 토큰 아끼기

--- app/page.tsx
+++ app/page.tsx
@@ -1,14 +1,14 @@
-// import insertAnswer from "@/util/functions/insertAnswer";
+import insertAnswer from "@/util/functions/insertAnswer";
 
 export default async function Home() {
 
-  // 오늘의 단어 업데이트
-  // await insertAnswer('포스트잇');
+  // 오늘의 단어 업데이트하는 문장
+  await insertAnswer('샌드위치');
 
   return (
     <form action="http://localhost:3000/api/get/checkAnswer" method="GET">
       <input name="answer" />
-      <button type="submit">보내기</button>
+      <button type="submit">추측하기</button>
     </form>
   );
 }
