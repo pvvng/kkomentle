8485b360b1da0bfece2bdb8bf89272431011f796
김동우 <t6u80o@naver.com>
2024-07-20 21:04:15 +0900

    f: toml 파일 수정,  netlify function 설정

--- netlify/functions/updateWord.ts
+++ netlify/functions/updateWord.ts
@@ -7,7 +7,7 @@ import { Handler } from '@netlify/functions';
  * 1. 매일 자정 실행되어 db에 오늘의 단어를 업데이트 
  * 2. db 유사도를 삭제/업데이트/추가 하는 함수 
  * */
-export const handler: Handler = async () => {
+export const handler: Handler = async (event, context) => {
   try {
 
     // 오늘의 단어 선별하는 API
