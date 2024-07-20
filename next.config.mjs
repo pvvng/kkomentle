e674029f16a6499cf9b14e1dc176fd3e9de53a86
김동우 <t6u80o@naver.com>
2024-07-20 20:42:32 +0900

    f: 빌드 테스트(s)

--- next.config.mjs
+++ next.config.mjs
@@ -3,6 +3,7 @@ const require = createRequire(import.meta.url);
 import nextPWA from 'next-pwa';
 
 const withPWA = nextPWA({
+  disable: process.env.NODE_ENV === 'development',
   dest: 'public',
   register: true,
   skipWaiting: true,
