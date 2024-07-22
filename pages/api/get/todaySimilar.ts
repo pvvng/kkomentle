621ae1631f0a77f308bc587bc889f959a5dc27a7
김동우 <t6u80o@naver.com>
2024-07-23 00:42:43 +0900

    f: handler로 api 이름 변경

--- pages/api/get/todaySimilar.ts
+++ pages/api/get/todaySimilar.ts
@@ -2,7 +2,7 @@ import { NextApiRequest, NextApiResponse } from "next";
 import { connectDB } from "@/util/database";
 import moment from "moment-timezone";
 
-export default async function getTodaySimilar(req :NextApiRequest, res :NextApiResponse){
+export default async function handler(req :NextApiRequest, res :NextApiResponse){
     // 디바이스 시간을 한국시로 포맷
     const userNowDate = new Date();
     const koreanNowDate = moment(userNowDate).tz("Asia/Seoul");
